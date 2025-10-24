371ca3568bd4e4f149c0cdc1881503eb0a769e2e
lewis617 <lewis617@163.com>
2025-10-24 16:48:43 +0800

    Remove 'Home on the Range' song

--- js/songs-index.js
+++ js/songs-index.js
@@ -1,8 +1,6 @@
 // 自动生成的歌曲索引文件
 
 import { song as stand_by_me } from './songs/stand-by-me.js';
-import { song as home_on_the_range } from './songs/home-on-the-range.js';
-
 import { song as you_raise_me_up } from './songs/you-raise-me-up.js';
 import { song as goodbye_to_romance } from './songs/goodbye-to-romance.js';
 import { song as yellow } from './songs/yellow.js';
@@ -14,7 +12,6 @@ import { song as cant_take_my_eyes_off_you } from './songs/cant-take-my-eyes-off
 // 导出所有歌曲数组
 export const songs = [
   stand_by_me,
-  home_on_the_range,
   you_raise_me_up,
   goodbye_to_romance,
   yellow,
